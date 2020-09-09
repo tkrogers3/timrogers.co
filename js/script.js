9437bb0dd05cbdfb653eb9056e09ca7c4d27918e
tkrogers3 <tkrogers3@gmail.com>
2020-09-09 12:23:56 -0400

    Added typed.js to subheader, icon buttons to change image.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,17 @@ $(document).ready(function(){
 
     $('#slides').superslides({
         animation:'fade',
-        play: 4000
+        play: 4000,
+        pagination: false
     });
+
+    var typed = new Typed(".typed",{
+        strings:["Software Engineer", "Web Developer","Project Builder"],
+        typeSpeed: 80,
+        loop: true,
+        startDelay:1000,
+        showCursor:false
+    });
+
 
 });
