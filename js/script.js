6133c676b77ec1cabd79a9312edba94f1056de76
tkrogers3 <tkrogers3@gmail.com>
2020-09-15 16:56:29 -0400

    Bootstrap styling for responsiveness.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
     $('#slides').superslides({
-        animation:'fade',
+        animation: 'fade',
         play: 4000,
         pagination: false
     });
 
-    var typed = new Typed(".typed",{
-        strings:["Software Engineer", "Web Developer","Project Builder"],
+    var typed = new Typed(".typed", {
+        strings: ["Software Engineer", "Web Developer", "App Programmer"],
         typeSpeed: 80,
         loop: true,
-        startDelay:1000,
-        showCursor:false
+        startDelay: 1000,
+        showCursor: false
+
     });
 
 
